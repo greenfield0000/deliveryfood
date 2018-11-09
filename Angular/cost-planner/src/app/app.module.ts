d1ad87c9f88e2e2f347c319d964918df9a4c7ee2
greenfield0000@mail.ru <greenfield0000@mail.ru>
2018-11-09 18:16:25 +0300

    Add sideNav

--- Angular/cost-planner/src/app/app.module.ts
+++ Angular/cost-planner/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatSidenavModule } from '@angular/material';
 import { FormsModule, } from '@angular/forms';
 import { LayoutModule } from '@angular/cdk/layout';
 import { HeaderOutletComponent } from './outlets/header-outlet/header-outlet.component';
@@ -27,11 +27,12 @@ import { FooterOutletComponent } from './outlets/footer-outlet/footer-outlet.com
     MatButtonModule,
     LayoutModule,
     MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    MatSidenavModule
   ],
-
+  exports: [
+    MatToolbarModule,
+    MatSidenavModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Angular/cost-planner/src/app/outlets/main-outlet/main-outlet.component.ts
+++ Angular/cost-planner/src/app/outlets/main-outlet/main-outlet.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainOutletComponent implements OnInit {
 
+  private _showFiller = false;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  public get showFilter(): boolean {
+    return this._showFiller;
+  }
+
+  public set showFilter(showFilter: boolean) {
+    this._showFiller = showFilter;
+  }
+
 
 }
