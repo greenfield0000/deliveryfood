e197bf7f000841ccbab8b29049bd408eaaf4f57f
greenfield0000@mail.ru <greenfield0000@mail.ru>
2019-01-03 05:27:26 +0300

    fix linl beetwen-container

--- backend/microservices/menu-service/src/main/java/greenfield/group/com/menuservice/MenuServiceApplication.java
+++ backend/microservices/menu-service/src/main/java/greenfield/group/com/menuservice/MenuServiceApplication.java
@@ -2,8 +2,12 @@ package greenfield.group.com.menuservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
+@EntityScan("entities")
 public class MenuServiceApplication {
 
 	public static void main(String[] args) {
